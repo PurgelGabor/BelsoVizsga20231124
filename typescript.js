f6b4cc02e5bf6544fb27a4442114ebd8c98b3c81
PurgelGabor <gaborpurgel0@gmail.com>
2023-11-25 00:21:55 +0100

    BelsoVizsga

---
+++ typescript.js
@@ -0,0 +1,16 @@
+function osszesOszto(szam) {
+    var i;
+    if (szam % i == 0) {
+        var osztok = [];
+        osztok.push(i);
+    }
+}
+function parosDarab(szam) {
+    for (var i = 0; szam.length; i++) {
+        if (szam[i] % 2 == 0) {
+            return szam[i];
+        }
+    }
+}
+function fuggvenyhivasPalindrom() {
+}
---
+++ typescript.ts
@@ -0,0 +1,19 @@
+function osszesOszto(szam:number){
+    var i:number;
+    if (szam % i == 0) {
+        var osztok = [];
+        osztok.push(i);
+    }
+}
+
+function parosDarab(szam:number[]){
+    for(var i:number = 0; szam.length; i++){
+        if(szam[i] % 2 == 0){
+            return szam[i];
+        }
+    }
+}
+
+function fuggvenyhivasPalindrom(){
+
+}
